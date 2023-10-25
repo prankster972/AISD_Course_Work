dc3cbfc6ef39143b806a2bb51cfabf0d706cf0ee
prankster972 <morozovroman.ist@gmail.com>
2023-10-25 16:00:04 +0400

    delete folder

--- AISD/AiSD_CourseWork/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AiSD_CourseWork.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
